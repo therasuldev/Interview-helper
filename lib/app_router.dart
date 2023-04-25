5f26221fedd2b12dba7cefe2aa24e9fc7a0e5d9e
therasuldev <rasul.ramixanov@gmail.com>
2023-04-25 22:31:11 +0400

    chore: cleaning

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_interview_questions/app_route_const.dart';
-import 'package:flutter_interview_questions/main.dart';
-import 'package:flutter_interview_questions/question_list_page.dart';
-import 'package:flutter_interview_questions/question_view.dart';
+import 'package:flutter_interview_questions/view/pages/categories/programming_lang_categories.dart';
+import 'package:flutter_interview_questions/view/pages/view/question_view.dart';
 import 'package:go_router/go_router.dart';
+import 'view/pages/questionList/question_list_page.dart';
 
 class AppRouter {
   GoRouter router = GoRouter(
@@ -10,7 +10,7 @@ class AppRouter {
       GoRoute(
         name: AppRouteConstant.home,
         path: '/',
-        builder: (context, state) => const ProgrammingLanguages(),
+        builder: (context, state) => const ProgrammingLanguageCategories(),
         routes: [
           GoRoute(
             name: AppRouteConstant.questionListView,
@@ -20,8 +20,7 @@ class AppRouter {
               GoRoute(
                 name: AppRouteConstant.questionView,
                 path: 'b',
-                builder: (context, state) =>  QuestionView(
-                ),
+                builder: (context, state) => const QuestionView(),
               ),
             ],
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_interview_questions/app_router.dart';
-import 'package:flutter_interview_questions/gen/assets.gen.dart';
-import 'package:flutter_interview_questions/question_view.dart';
-import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,90 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: AppRouter().router,
-    );
-  }
-}
-
-class ProgrammingLanguages extends StatefulWidget {
-  const ProgrammingLanguages({super.key});
-
-  @override
-  State<ProgrammingLanguages> createState() => _ProgrammingLanguagesState();
-}
-
-class _ProgrammingLanguagesState extends State<ProgrammingLanguages> {
-  //* programming lang. images
-  final List<String> _langPNGs = [
-    Assets.programmingLangPng.flutter.path,
-    Assets.programmingLangPng.go.path,
-  ];
-
-  //* programming lang. names
-  final List<String> _langNames = [
-    'Flutter',
-    'Go lang',
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: GridView.builder(
-        itemCount: _langPNGs.length,
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 250,
-          childAspectRatio: 1,
-          mainAxisSpacing: 30,
-        ),
-        itemBuilder: (_, i) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: GestureDetector(
-              onTap: () {
-                context.go('/listv');
-              },
-              child: Container(
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.grey,
-                      offset: Offset(0, 10),
-                      spreadRadius: .5,
-                      blurRadius: 10,
-                    )
-                  ],
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Colors.white,
-                      Colors.blueGrey[200]!,
-                    ],
-                    stops: const [0.5, 1],
-                  ),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Spacer(),
-                    SizedBox(height: 100, child: Image.asset(_langPNGs[i])),
-                    const Spacer(),
-                    Text(
-                      _langNames[i],
-                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                            color: Colors.grey[600]!.withRed(50),
-                            fontWeight: FontWeight.w900,
-                          ),
-                    ),
-                    const Spacer(),
-                  ],
-                ),
-              ),
-            ),
-          );
-        },
-      ),
     );
   }
 }
