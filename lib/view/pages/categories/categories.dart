e08d8caa7207aeec45d87bbddb04370c609dcfc0
therasuldev <rasul.ramixanov@gmail.com>
2023-05-10 23:21:09 +0400

    bug fix: fixed some issues

--- lib/view/pages/categories/categories.dart
+++ lib/view/pages/categories/categories.dart
@@ -4,7 +4,7 @@ import 'package:flutter_interview_questions/app_navigators.dart';
 import 'package:flutter_interview_questions/core/provider/question_bloc/question_bloc.dart';
 import 'package:flutter_interview_questions/core/provider/question_bloc/question_event.dart';
 import 'package:flutter_interview_questions/core/provider/question_bloc/question_state.dart';
-import 'package:flutter_interview_questions/gen/assets.gen.dart';
+import 'package:flutter_interview_questions/core/utils/categories.dart';
 import 'package:flutter_interview_questions/view/pages/categories/questions/questions.dart';
 import 'package:flutter_interview_questions/view/utils/utils.dart';
 
@@ -16,39 +16,6 @@ class LangCategories extends StatefulWidget {
 }
 
 class _LangCategoriesState extends State<LangCategories> {
-  final List<String> _images = [
-    Assets.programmingLangPng.flutter.path,
-    Assets.programmingLangPng.go.path,
-    Assets.programmingLangPng.python.path,
-    Assets.programmingLangPng.ruby.path,
-    Assets.programmingLangPng.rust.path,
-    Assets.programmingLangPng.java.path,
-    Assets.programmingLangPng.react.path,
-    Assets.programmingLangPng.js.path,
-  ];
-
-  final List<String> languages = [
-    'Flutter',
-    'Go Lang',
-    'Python',
-    'Ruby',
-    'Rust',
-    'Java',
-    'Java Script',
-    'React',
-  ];
-
-  final List<String> categories = [
-    'flutter',
-    'go',
-    'python',
-    'ruby',
-    'rust',
-    'java',
-    'js',
-    'react',
-  ];
-
   @override
   void initState() {
     super.initState();
@@ -69,40 +36,56 @@ class _LangCategoriesState extends State<LangCategories> {
           }
         },
         child: GridView.builder(
-          itemCount: categories.length,
           physics: const BouncingScrollPhysics(),
           gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
             maxCrossAxisExtent: 250,
             childAspectRatio: 1,
             mainAxisSpacing: 30,
           ),
-          itemBuilder: (context, index) => GestureDetector(
-            onTap: () => _bloc.add(
-              QuestionEvent.fetchQuestionStart(categories[index]),
-            ),
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 10),
-              alignment: Alignment.center,
-              decoration: ViewUtils.categoryCard(),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Spacer(),
-                  SizedBox(height: 100, child: Image.asset(_images[index])),
-                  const Spacer(),
-                  Text(
-                    languages[index],
-                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                          color: Colors.grey[600]!.withRed(50),
-                          fontWeight: FontWeight.w900,
-                        ),
-                  ),
-                  const Spacer(),
-                ],
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () => _bloc.add(
+                QuestionEvent.fetchQuestionStart(
+                  Categories.categories[index],
+                ),
               ),
+              child: _CategoryCard(
+                image: Categories.images[index],
+                language: Categories.names[index],
+              ),
+            );
+          },
+          itemCount: Categories.categories.length,
+        ),
+      );
+}
+
+class _CategoryCard extends StatelessWidget {
+  const _CategoryCard({required this.image, required this.language});
+
+  final String image;
+  final String language;
+
+  @override
+  Widget build(BuildContext context) => Container(
+        margin: const EdgeInsets.symmetric(horizontal: 10),
+        alignment: Alignment.center,
+        decoration: ViewUtils.categoryCard(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(),
+            SizedBox(height: 100, child: Image.asset(image)),
+            const Spacer(),
+            Text(
+              language,
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Colors.grey[600]!.withRed(50),
+                  fontWeight: FontWeight.w900),
             ),
-          ),
+            const Spacer(),
+          ],
         ),
       );
 }
