964c0614209bcdd6d2d5b7908496b65fd9e40417
Rasul Ramixanov <74558294+therasuldev@users.noreply.github.com>
2023-05-25 14:11:12 +0400

    feat: fetch data from FirebaseStorage

--- lib/view/pages/library/library.dart
+++ lib/view/pages/library/library.dart
@@ -1,6 +1,9 @@
+import 'package:dio/dio.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_interview_questions/gen/assets.gen.dart';
 import 'package:flutter_interview_questions/view/utils/utils.dart';
+import 'package:path_provider/path_provider.dart';
 
 class BookStore extends StatefulWidget {
   const BookStore({super.key});
@@ -10,19 +13,96 @@ class BookStore extends StatefulWidget {
 }
 
 class _BookStoreState extends State<BookStore> {
+  Map<int, double> downloadProgress = {};
+  late Future<ListResult> futureFiles;
+  bool isCompleted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    futureFiles = FirebaseStorage.instance.ref('/golang').listAll();
+  }
+
+  Future downloadFile(int index, Reference ref) async {
+    final url = await ref.getDownloadURL();
+
+    final temp = await getTemporaryDirectory();
+    final path = '${temp.path}/${ref.name}';
+
+    await Dio().download(
+      url,
+      path,
+      onReceiveProgress: (count, total) {
+        double progress = count / total;
+        setState(() => downloadProgress[index] = progress);
+      },
+    ).whenComplete(() => setState(() => isCompleted = true));
+  }
+
+  Widget? subtitleWidget(double? progress) {
+    if (progress == null) return null;
+    if (!isCompleted) return LinearProgressIndicator(value: progress);
+    return null;
+  }
+
+  Widget? downloadIcon(int index, Reference ref, double? progress) {
+    if (!isCompleted && progress == null) {
+      return IconButton(
+        onPressed: () => downloadFile(index, ref),
+        icon: const Icon(Icons.download),
+      );
+    }
+    if (!isCompleted && progress != null) {
+      return Container(
+        height: 50,
+        width: 50,
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(shape: BoxShape.circle),
+        child: Text((progress * 100).round().toString()),
+      );
+    }
+    return const Icon(Icons.download_done_outlined);
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: const [
-        _RowTitleWidget(title: 'Flutter'),
-        _BookCardWidget(),
+      shrinkWrap: true,
+      children: [
+        _RowTitleWidget(title: 'Flutter', onPressed: () {}),
+        const _BookCardWidget(),
         //*
-        _RowTitleWidget(title: 'Go Lang'),
-        _BookCardWidget(),
+        _RowTitleWidget(title: 'Go Lang', onPressed: () {}),
+        const _BookCardWidget(),
       ],
     );
   }
 }
+
+// FutureBuilder<ListResult>(
+//           future: futureFiles,
+//           builder: (context, snapshot) {
+//             if (snapshot.hasData) {
+//               final files = snapshot.data!.items;
+//               return ListView.builder(
+//                 shrinkWrap: true,
+//                 itemCount: files.length,
+//                 itemBuilder: (context, index) {
+//                   final file = files[index];
+//                   final double? progress = downloadProgress[index];
+//                   return ListTile(
+//                       title: Text(file.name),
+//                       subtitle: subtitleWidget(progress),
+//                       trailing: downloadIcon(index, file, progress));
+//                 },
+//               );
+//             } else if (snapshot.hasError) {
+//               return const Center(child: Text('error occured'));
+//             } else {
+//               return const SizedBox.shrink();
+//             }
+//           },
+//         ),
 
 class _BookCardWidget extends StatelessWidget {
   const _BookCardWidget();
@@ -74,9 +154,10 @@ class _BookCardWidget extends StatelessWidget {
 }
 
 class _RowTitleWidget extends StatelessWidget {
-  const _RowTitleWidget({required this.title});
+  const _RowTitleWidget({required this.title, required this.onPressed});
 
   final String title;
+  final void Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -85,7 +166,7 @@ class _RowTitleWidget extends StatelessWidget {
       children: [
         Text(title, style: ViewUtils.ubuntuStyle(fontSize: 20)),
         TextButton(
-          onPressed: () {},
+          onPressed: onPressed,
           child: Text('All', style: ViewUtils.ubuntuStyle(fontSize: 20)),
         )
       ],
