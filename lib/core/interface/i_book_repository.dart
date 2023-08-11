fb66ca120603f09777d44bdb9b140efd7c78cb4a
therasuldev <rasul.ramixanov@gmail.com>
2023-08-12 02:42:15 +0400

    new func getSingleFile

--- lib/core/interface/i_book_repository.dart
+++ lib/core/interface/i_book_repository.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_interview_questions/core/app/enum/kpath_event.dart';
+import 'package:flutter_interview_questions/core/model/book/book.dart';
 
 abstract class IBookRepository {
-  Future fetchBooks(Set<Path> path);
+  Future<Set<Map<String, ListResult?>>> fetchBooks(Set<Path> path);
+  Future<File> getFirstpage(Book book);
 }
