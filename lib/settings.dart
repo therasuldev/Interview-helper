c2c8621178e3f85febb9db0970bb7d6449bcce5c
therasuldev <rasul.ramixanov@gmail.com>
2023-09-09 00:03:23 +0400

    impl: notifications button

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:interview_prep/settings_tile.dart';
 import 'package:interview_prep/view/utils/utils.dart';
@@ -11,6 +12,7 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  bool switchVal = false;
   String cacheSize = '';
   late final SimpleAppCacheManager cacheManager;
 
@@ -27,42 +29,55 @@ class _SettingsState extends State<Settings> {
     updateCacheSize();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        padding: EdgeInsets.symmetric(
-          vertical: MediaQuery.of(context).padding.top,
-          horizontal: 15,
-        ),
-        children: [
-          const SizedBox(height: 10),
-          Text('Settings', style: ViewUtils.ubuntuStyle(fontSize: 25)),
-          const SizedBox(height: 10),
-          SettingTile(
-            icon: const Icon(
-              size: 20,
-              color: Colors.white,
-              Icons.edit_attributes_rounded,
-            ),
-            tralling: Text(
-              cacheSize.toString(),
-              style: ViewUtils.ubuntuStyle(color: Colors.red),
-            ),
-            onTap: () async {
-              cacheManager.clearCache();
-              updateCacheSize();
-            },
-            title: 'Clear cache',
-            iconColor: Colors.orange,
-          ),
-        ],
-      ),
-    );
-  }
-
   void updateCacheSize() async {
     final newSize = await cacheManager.getTotalCacheSize();
     setState(() => cacheSize = newSize);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          children: [
+            const SizedBox(height: 10),
+            Text('Settings', style: ViewUtils.ubuntuStyle(fontSize: 25)),
+            const SizedBox(height: 10),
+            SettingTile(
+              icon: const Icon(
+                size: 20,
+                color: Colors.white,
+                Icons.edit_attributes_rounded,
+              ),
+              tralling: Text(
+                cacheSize.toString(),
+                style: ViewUtils.ubuntuStyle(color: Colors.red, fontSize: 19),
+              ),
+              onTap: () async {
+                cacheManager.clearCache();
+                updateCacheSize();
+              },
+              title: 'Clear cache',
+              iconColor: Colors.orange,
+            ),
+            SettingTile(
+              icon: const Icon(
+                size: 20,
+                color: Colors.white,
+                Icons.notifications,
+              ),
+              tralling: CupertinoSwitch(
+                value: switchVal,
+                activeColor: Colors.green,
+                onChanged: (val) => setState(() => switchVal = val),
+              ),
+              title: 'Notifications',
+              iconColor: Colors.blueGrey,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
