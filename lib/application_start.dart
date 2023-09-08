2ebb6bd7f12ef9caa0bdceeadd107aace0b8935f
therasuldev <rasul.ramixanov@gmail.com>
2023-09-09 00:05:20 +0400

    bug: FlutterJNI.init called more than once

--- lib/application_start.dart
+++ lib/application_start.dart
@@ -2,22 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:interview_prep/firebase_options.dart';
 import 'package:interview_prep/utils/hive_utils.dart';
-import 'package:interview_prep/utils/work_manager_utils.dart';
-import 'package:workmanager/workmanager.dart';
 
 class Application {
   const Application._();
 
   static start() async {
     WidgetsFlutterBinding.ensureInitialized();
-
-    //Background local notifications service
-    await Workmanager().initialize(WorkmanagerUtils.callbackDispatcher);
-    await Workmanager().registerPeriodicTask(
-      '2',
-      'Interview Questions App',
-      frequency: const Duration(days: 5),
-    );
 
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,23 @@ import 'package:interview_prep/core/app/enum/kpath_event.dart';
 import 'package:interview_prep/core/provider/books_bloc/books_bloc.dart';
 import 'package:interview_prep/core/provider/feedback_cubit/cubit/feedback_cubit.dart';
 import 'package:interview_prep/core/provider/question_bloc/question_bloc.dart';
+import 'package:interview_prep/utils/work_manager_utils.dart';
 import 'package:interview_prep/view/general_page.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:workmanager/workmanager.dart';
 
-Future<void> main() async {
+void main() async {
   await Application.start();
   await initialization();
+
+  //Background local notifications service
+  await Workmanager().initialize(WorkmanagerUtils.callbackDispatcher);
+  await Workmanager().registerPeriodicTask(
+    '2',
+    'Interview Questions App',
+    frequency: const Duration(days: 5),
+  );
+
   runApp(const MyApp());
 }
 
