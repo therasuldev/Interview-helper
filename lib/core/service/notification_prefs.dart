ef987afa67a009a1e100055d6f9c3c7bb0914a64
therasuldev <rasul.ramixanov@gmail.com>
2023-09-17 02:37:29 +0400

    feat: service for [notifications] && [connectivity]

---
+++ lib/core/service/notification_prefs.dart
@@ -0,0 +1,15 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class NotificationPrefs {
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  void notificationCtrlSet(bool isEnabled) async {
+    (await _prefs).setBool('notification', isEnabled);
+  }
+  
+  /// if it returns true, it means notification settings are enabled.
+  Future<bool?> notificationCtrlGet() async {
+    final result = (await _prefs).getBool('notification');
+    return result;
+  }
+}
