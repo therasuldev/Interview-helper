c2320d2ad4d0df9f0fa404c9a891bd3962698a9e
therasuldev <rasul.ramixanov@gmail.com>
2023-09-16 23:10:14 +0400

    feat: connectivity check

--- lib/contact_us_screen.dart
+++ lib/contact_us_screen.dart
@@ -2,21 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:interview_prep/app_colors.dart';
 import 'package:interview_prep/core/model/email/emailjs.dart';
-import 'package:interview_prep/core/provider/feedback_cubit/feedback_cubit.dart';
+import 'package:interview_prep/core/provider/cubit/feedback/feedback_cubit.dart';
+import 'package:interview_prep/core/service/network_manager.dart';
 import 'package:interview_prep/form_validate.dart';
 import 'package:interview_prep/spinkit_circle_loading_widget.dart';
 import 'package:interview_prep/view/utils/utils.dart';
 
-class ContactUsScreen extends StatelessWidget {
-  final TextEditingController _message = TextEditingController();
-  final TextEditingController _email = TextEditingController();
+class ContactUsScreen extends StatefulWidget {
+  const ContactUsScreen({super.key});
+  
+  @override
+  State<ContactUsScreen> createState() => _ContactUsScreenState();
+}
 
-  final _formKey = GlobalKey<FormState>();
-  final _emailKey = GlobalKey<FormFieldState>();
-  final _feedbackKey = GlobalKey<FormFieldState>();
+class _ContactUsScreenState extends State<ContactUsScreen> {
+  final TextEditingController message = TextEditingController();
+  final TextEditingController email = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  final emailKey = GlobalKey<FormFieldState>();
+  final feedbackKey = GlobalKey<FormFieldState>();
 
-  ContactUsScreen({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -24,7 +30,7 @@ class ContactUsScreen extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
           child: Form(
-            key: _formKey,
+            key: formKey,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -35,8 +41,8 @@ class ContactUsScreen extends StatelessWidget {
                 const SizedBox(height: 20),
                 TextFormField(
                   maxLines: 1,
-                  key: _emailKey,
-                  controller: _email,
+                  key: emailKey,
+                  controller: email,
                   style: ViewUtils.ubuntuStyle(),
                   textInputAction: TextInputAction.done,
                   keyboardType: TextInputType.emailAddress,
@@ -54,8 +60,8 @@ class ContactUsScreen extends StatelessWidget {
                 const SizedBox(height: 20),
                 TextFormField(
                   maxLines: 7,
-                  key: _feedbackKey,
-                  controller: _message,
+                  key: feedbackKey,
+                  controller: message,
                   style: ViewUtils.ubuntuStyle(),
                   textInputAction: TextInputAction.done,
                   keyboardType: TextInputType.text,
@@ -73,9 +79,9 @@ class ContactUsScreen extends StatelessWidget {
                 const Spacer(),
                 Center(
                   child: _SendFeedbackButton(
-                    email: _email,
-                    message: _message,
-                    formKey: _formKey,
+                    email: email,
+                    message: message,
+                    formKey: formKey,
                   ),
                 ),
               ],
@@ -89,14 +95,13 @@ class ContactUsScreen extends StatelessWidget {
 
 class _SendFeedbackButton extends StatelessWidget {
   const _SendFeedbackButton({
-    required TextEditingController email,
-    required TextEditingController message,
+    required this.email,
+    required this.message,
     required this.formKey,
-  })  : _email = email,
-        _message = message;
+  }) : super();
 
-  final TextEditingController _email;
-  final TextEditingController _message;
+  final TextEditingController email;
+  final TextEditingController message;
 
   final GlobalKey<FormState> formKey;
 
@@ -105,55 +110,62 @@ class _SendFeedbackButton extends StatelessWidget {
     return SizedBox(
       height: 50,
       width: MediaQuery.of(context).size.width * .9,
-      child: ElevatedButton(
-        onPressed: () {
-          final isValidate = formKey.currentState?.validate() ?? false;
-          if (!isValidate) return;
-          final msgParams =
-              MSGParams(email: _email.text, message: _message.text);
-          context.read<FeedbackCubit>().send(msgParams: msgParams);
+      child: StreamBuilder(
+        stream: ConnectivityService().controller.stream,
+        builder: (context, snapshot) {
+          return ElevatedButton(
+            onPressed: () {
+              final isValidate = formKey.currentState?.validate() ?? false;
+              if (!isValidate && snapshot.data != ConnectivityStatus.online) {
+                return;
+              }
+              final msgParams =
+                  MSGParams(email: email.text, message: message.text);
+              context.read<FeedbackCubit>().send(msgParams: msgParams);
+            },
+            style: ButtonStyle(
+              elevation: MaterialStateProperty.all(0),
+              backgroundColor: MaterialStateProperty.all(Colors.white),
+              foregroundColor: MaterialStateProperty.all(AppColors.appColor),
+              side: MaterialStateProperty.all(
+                  const BorderSide(color: AppColors.appColor, strokeAlign: 10)),
+            ),
+            child: BlocConsumer<FeedbackCubit, FeedbackState>(
+              listener: (context, state) {
+                switch (state.event) {
+                  case FeedbackEvents.success:
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        margin: const EdgeInsets.all(10),
+                        backgroundColor: Colors.green,
+                        behavior: SnackBarBehavior.floating,
+                        content: Text('Thank you for your feedback.',
+                            style: ViewUtils.ubuntuStyle(fontSize: 12)),
+                      ),
+                    );
+                    break;
+                  case FeedbackEvents.fail:
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        backgroundColor: Colors.red,
+                        margin: const EdgeInsets.all(10),
+                        behavior: SnackBarBehavior.floating,
+                        content: Text(state.exception!.description,
+                            style: ViewUtils.ubuntuStyle()),
+                      ),
+                    );
+                    break;
+                  default:
+                    break;
+                }
+              },
+              builder: (context, state) {
+                if (state.loading) return const KSpinKitCircle();
+                return Text('Send feedback', style: ViewUtils.ubuntuStyle());
+              },
+            ),
+          );
         },
-        style: ButtonStyle(
-          elevation: MaterialStateProperty.all(0),
-          backgroundColor: MaterialStateProperty.all(Colors.white),
-          foregroundColor: MaterialStateProperty.all(AppColors.appColor),
-          side: MaterialStateProperty.all(
-              const BorderSide(color: AppColors.appColor, strokeAlign: 10)),
-        ),
-        child: BlocConsumer<FeedbackCubit, FeedbackState>(
-          listener: (context, state) {
-            switch (state.event) {
-              case FeedbackEvents.success:
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    margin: const EdgeInsets.all(10),
-                    backgroundColor: Colors.green,
-                    behavior: SnackBarBehavior.floating,
-                    content: Text('Thank you for your feedback.',
-                        style: ViewUtils.ubuntuStyle(fontSize: 12)),
-                  ),
-                );
-                break;
-              case FeedbackEvents.fail:
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    backgroundColor: Colors.red,
-                    margin: const EdgeInsets.all(10),
-                    behavior: SnackBarBehavior.floating,
-                    content: Text(state.exception!.description,
-                        style: ViewUtils.ubuntuStyle()),
-                  ),
-                );
-                break;
-              default:
-                break;
-            }
-          },
-          builder: (context, state) {
-            if (state.loading) return const KSpinKitCircle();
-            return Text('Send feedback', style: ViewUtils.ubuntuStyle());
-          },
-        ),
       ),
     );
   }
