b3526d76c4073d1b638b5f4010a59cef5b88ac11
therasuldev <rasul.ramixanov@gmail.com>
2023-12-19 01:34:46 +0400

    feat: open pdf page completed

--- lib/src/presentation/views/library_screen/open_pdf_view.dart
+++ lib/src/presentation/views/library_screen/open_pdf_view.dart
@@ -1,18 +1,131 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:interview_helper/src/utils/decorations/view_utils.dart';
 
-import '../../../domain/models/models.dart';
-import '../../widgets/widgets.dart';
+class OpenPDFView extends StatefulWidget {
+  final String filePath;
 
-class OpenPDFView extends StatelessWidget {
-  const OpenPDFView({super.key, required this.book});
+  const OpenPDFView({super.key, required this.filePath});
 
-  final Book book;
+  @override
+  State<OpenPDFView> createState() => _OpenPDFViewState();
+}
+
+class _OpenPDFViewState extends State<OpenPDFView> {
+  int currentPage = 0;
+  PDFViewController? pdfViewController;
+  final controller = TextEditingController();
+
+  void setPage(int page) {
+    setState(() => currentPage = page);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: BookViewModel(book: book).buildBookforEntireScreen(context),
+      appBar: AppBar(
+        actions: [
+          _PageInputField(controller: controller),
+          _NavigationButton(
+            controller: controller,
+            pdfViewController: pdfViewController,
+            setPage: setPage,
+          ),
+        ],
+      ),
+      body: PDFView(
+        filePath: widget.filePath,
+        enableSwipe: true,
+        swipeHorizontal: false,
+        autoSpacing: false,
+        pageFling: false,
+        defaultPage: currentPage,
+        onViewCreated: (PDFViewController controller) {
+          pdfViewController = controller;
+        },
+        onPageChanged: (int? page, int? total) {
+          setState(() => currentPage = page ?? 0);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          if (pdfViewController != null) {
+            setPage(currentPage + 1);
+            await pdfViewController!.setPage(currentPage);
+          }
+        },
+        child: const Icon(
+          Icons.arrow_downward,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
+class _PageInputField extends StatelessWidget {
+  const _PageInputField({required this.controller});
+
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 60,
+      height: 40,
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          label: Text(
+            'Page',
+            style: ViewUtils.ubuntuStyle(
+              color: Colors.grey.shade300,
+              fontSize: 12,
+            ),
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade300),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade300),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade300),
+          ),
+        ),
+        keyboardType: TextInputType.number,
+        inputFormatters: [
+          FilteringTextInputFormatter.allow(RegExp(r'^\d*$')),
+        ],
+      ),
+    );
+  }
+}
+
+class _NavigationButton extends StatelessWidget {
+  final TextEditingController controller;
+  final PDFViewController? pdfViewController;
+  final Function(int) setPage;
+
+  const _NavigationButton({
+    required this.controller,
+    required this.pdfViewController,
+    required this.setPage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () async {
+        if (pdfViewController != null) {
+          int page = int.tryParse(controller.value.text.trim()) ?? (await pdfViewController!.getCurrentPage())!;
+          setPage(page);
+          await pdfViewController!.setPage(page);
+        }
+      },
+      icon: const Icon(Icons.arrow_forward),
     );
   }
 }
