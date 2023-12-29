a18894378739b8e821d2b1ca40faa67a6b40aa70
therasuldev <rasul.ramixanov@gmail.com>
2023-12-29 23:33:18 +0400

    feat: hive config

---
+++ lib/src/config/hive/hive_config.dart
@@ -0,0 +1,29 @@
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart' as path;
+
+import '../../domain/models/models.dart';
+
+class HiveConfig {
+  HiveConfig._();
+
+  static final HiveConfig _config = HiveConfig._();
+  static HiveConfig get config => _config;
+
+  Future<void> init() async {
+    final dir = await path.getApplicationDocumentsDirectory();
+    await Hive.initFlutter(dir.path);
+
+    _registerAdapters();
+    await _openBoxes();
+  }
+
+  void _registerAdapters() {
+    if (!Hive.isAdapterRegistered(0)) {
+      Hive.registerAdapter(QuestionAdapter());
+    }
+  }
+
+  Future<void> _openBoxes() async {
+    await Hive.openBox('questions');
+  }
+}
