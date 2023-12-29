10ab8a5467b266afadab6c4a74dd69c4ec8ea2e8
therasuldev <rasul.ramixanov@gmail.com>
2023-12-29 23:35:09 +0400

    feat: theme config

---
+++ lib/src/config/theme/theme_config.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:interview_helper/src/utils/constants/app_colors.dart';
+import 'package:interview_helper/src/utils/decorations/view_utils.dart';
+
+class ThemeConfig {
+  ThemeConfig._();
+
+  static final ThemeConfig _init = ThemeConfig._();
+  static ThemeConfig get init => _init;
+
+  final ThemeData theme = ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: AppBarTheme(
+      iconTheme: const IconThemeData(color: Colors.white),
+      elevation: 0,
+      centerTitle: true,
+      backgroundColor: AppColors.primary,
+      titleTextStyle: ViewUtils.ubuntuStyle(fontSize: 22),
+    ),
+    drawerTheme: DrawerThemeData(
+      surfaceTintColor: Colors.grey.shade600,
+      backgroundColor: Colors.white,
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: AppColors.primary,
+    ),
+  );
+}
