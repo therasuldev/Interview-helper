11229b7e01cf9f121674f34217a972699b173585
therasuldev <rasul.ramixanov@gmail.com>
2023-12-29 23:36:48 +0400

    feat: fetch books from firebase

--- lib/src/data/datasources/remote/books_source_data_source.dart
+++ lib/src/data/datasources/remote/books_source_data_source.dart
@@ -1,26 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:interview_helper/src/utils/index.dart';
 
-import '../../../utils/enum/enums.dart';
+typedef BookJsonList = List<Map<String, dynamic>>;
 
 abstract class BooksSourceData {
-  Future<Set<Map<String, List<Map<String, dynamic>>?>>> getBooksSources();
+  Future<Set<Map<String, BookJsonList?>>> getBooksSources();
 }
 
 class BooksSourceDataImpl implements BooksSourceData {
   @override
-  Future<Set<Map<String, List<Map<String, dynamic>>?>>> getBooksSources() async {
-    final Set<Map<String, List<Map<String, dynamic>>?>> bookSources = {};
-    final List<Map<String, dynamic>> bookJsons = [];
+  Future<Set<Map<String, BookJsonList?>>> getBooksSources() async {
+    final Set<Map<String, BookJsonList?>> bookSources = {};
 
-    final List<Future<List<DocumentSnapshot>>> futures = Path.values.map((collectionName) {
-      return FirebaseFirestore.instance.collection(collectionName.name).get().then((querySnapshot) => querySnapshot.docs);
+    final List<Future<List<DocumentSnapshot>>> futures = BookCollectionPaths.values.map((collectionName) {
+      return FirebaseFirestore.instance.collection(collectionName.name).get().then((querySnapshot) {
+        return querySnapshot.docs;
+      });
     }).toList();
 
     final List<List<DocumentSnapshot>> results = await Future.wait(futures);
 
-    for (int i = 0; i < Path.values.length; i++) {
-      final String collectionName = Path.values[i].name;
+    for (int i = 0; i < BookCollectionPaths.values.length; i++) {
+      final String collectionName = BookCollectionPaths.values[i].name;
       final List<DocumentSnapshot> documents = results[i];
+      final List<Map<String, dynamic>> bookJsons = [];
 
       for (var document in documents) {
         bookJsons.add(document.data() as Map<String, dynamic>);
