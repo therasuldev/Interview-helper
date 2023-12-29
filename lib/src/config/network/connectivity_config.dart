edb9ce69783c340c422a73d4b96f5dff9b3fde20
therasuldev <rasul.ramixanov@gmail.com>
2023-12-29 23:33:42 +0400

    feat: network config

--- lib/src/config/network/connectivity_config.dart
+++ lib/src/config/network/connectivity_config.dart
@@ -1,7 +1,7 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 
 class ConnectivityService {
-  Future<bool> getConnectivityStatus() async {
+  Future<bool> hasActiveInternetConnection() async {
     final isConnected = await Connectivity().checkConnectivity().then((result) {
       return switch (result) {
         ConnectivityResult.wifi || ConnectivityResult.mobile => true,
