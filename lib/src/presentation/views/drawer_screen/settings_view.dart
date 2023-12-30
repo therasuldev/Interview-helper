9978016a0aaf0d2121fd5d86e077310884279f7a
therasuldev <rasul.ramixanov@gmail.com>
2023-12-30 23:59:38 +0400

    bugfix: check notification permission status when page opened

--- lib/src/presentation/views/drawer_screen/settings_view.dart
+++ lib/src/presentation/views/drawer_screen/settings_view.dart
@@ -1,5 +1,4 @@
 import 'package:app_settings/app_settings.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_app_cache_manager/simple_app_cache_manager.dart';
 import 'package:version_tracker/version_tracker.dart';
