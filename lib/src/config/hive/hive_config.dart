06815c9991c6ac5b4384473161fb328469c077e1
therasuldev <rasul.ramixanov@gmail.com>
2024-03-03 23:25:57 +0400

    new adapters

--- lib/src/config/hive/hive_config.dart
+++ lib/src/config/hive/hive_config.dart
@@ -1,4 +1,6 @@
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:interview_helper/src/domain/models/book/book_category.dart';
+import 'package:interview_helper/src/domain/models/question/question_category.dart';
 import 'package:path_provider/path_provider.dart' as path;
 
 import '../../domain/models/index.dart';
@@ -12,6 +14,8 @@ class HiveConfig {
   Future<void> init() async {
     final dir = await path.getApplicationDocumentsDirectory();
     await Hive.initFlutter(dir.path);
+    // Hive.deleteBoxFromDisk('question-categories');
+    // Hive.deleteBoxFromDisk('book-categories');
 
     _registerAdapters();
     await _openBoxes();
@@ -19,11 +23,23 @@ class HiveConfig {
 
   void _registerAdapters() {
     if (!Hive.isAdapterRegistered(0)) {
+      Hive.registerAdapter(BookCategoryAdapter());
+    }
+    if (!Hive.isAdapterRegistered(1)) {
+      Hive.registerAdapter(BookAdapter());
+    }
+
+    if (!Hive.isAdapterRegistered(2)) {
+      Hive.registerAdapter(QuestionCategoryAdapter());
+    }
+    if (!Hive.isAdapterRegistered(3)) {
       Hive.registerAdapter(QuestionAdapter());
     }
   }
 
   Future<void> _openBoxes() async {
     await Hive.openBox('questions');
+    await Hive.openBox<QuestionCategory>('question-categories');
+    await Hive.openBox<BookCategory>('book-categories');
   }
 }
