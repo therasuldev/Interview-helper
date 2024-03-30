b616837682d1896c469e92a308b69e3d4815c0ef
therasuldev <rasul.ramixanov@gmail.com>
2024-03-30 18:29:50 +0400

    added easy_localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:interview_helper/src/config/notification/one_signal_config.dart';
+import 'package:interview_helper/src/presentation/provider/cubit/language/language_cubit.dart';
 
 import 'src/config/app/app_config.dart';
 
@@ -20,6 +24,7 @@ import 'src/utils/index.dart';
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await EasyLocalization.ensureInitialized();
 
   // .env config
   await dotenv.load(fileName: ".env");
@@ -31,7 +36,14 @@ void main() async {
   OneSignalConfig.config.init();
 
   FlutterNativeSplash.remove();
-  runApp(const InterviewHelper());
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en', ''), Locale('tr', ''), Locale('ru', '')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en', ''),
+      child: const InterviewHelper(),
+    ),
+  );
 }
 
 class InterviewHelper extends StatelessWidget {
@@ -43,17 +55,26 @@ class InterviewHelper extends StatelessWidget {
       providers: [
         BlocProvider(create: (context) => AppBloc()..add(AppEvent.get())),
         BlocProvider(create: (context) => FeedbackCubit()),
+        BlocProvider(create: (context) => LanguageCubit()..loadLanguage()),
         BlocProvider(create: (context) => QuestionBloc()),
         BlocProvider(create: (context) => BookBloc()..add(BookEvent.fetchAllBooksStart())),
         BlocProvider(create: (context) => CategoryBloc()),
       ],
-      child: MaterialApp.router(
-        title: 'Interview Helper App',
-        restorationScopeId: 'interview_helper',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeConfig.init.theme,
-        routerConfig: AppRouterConfig.init.config,
-        scaffoldMessengerKey: ViewUtils.scaffoldMessengerKey,
+      child: BlocBuilder<AppBloc, AppState>(
+        builder: (context, state) {
+          if (state.loading) return const SizedBox.shrink();
+          return MaterialApp.router(
+            title: 'Interview Helper App',
+            restorationScopeId: 'interview_helper',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeConfig.init.theme,
+            routerConfig: AppRouterConfig.init.config,
+            scaffoldMessengerKey: ViewUtils.scaffoldMessengerKey,
+            locale: !state.onboardingViewed! ? View.of(context).platformDispatcher.locale : context.locale,
+            supportedLocales: context.supportedLocales,
+            localizationsDelegates: context.localizationDelegates,
+          );
+        },
       ),
     );
   }
