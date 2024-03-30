b1e93e002fb089fecfb4e8c5360e4df77f29d4e6
therasuldev <rasul.ramixanov@gmail.com>
2024-03-30 18:28:31 +0400

    change questions by lang

--- lib/src/presentation/views/home_screen/home_view.dart
+++ lib/src/presentation/views/home_screen/home_view.dart
@@ -1,6 +1,8 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:interview_helper/src/data/datasources/local/application_lang.dart';
 import 'package:interview_helper/src/presentation/provider/bloc/questions/question_bloc.dart';
 import 'package:interview_helper/src/utils/index.dart';
 
@@ -15,14 +17,18 @@ class _HomeViewState extends State<HomeView> {
   @override
   void initState() {
     super.initState();
-
     _bloc = BlocProvider.of<QuestionBloc>(context);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
     addQuestionsToCache();
   }
 
-  void addQuestionsToCache() {
+  void addQuestionsToCache() async {
     for (var category in InterviewCategories.categories.toList()) {
-      _bloc.add(QuestionEvent.addQuestionsInitial(category));
+      _bloc.add(QuestionEvent.addQuestionsInitial(category, context.locale.languageCode));
     }
   }
 
@@ -47,10 +53,11 @@ class _HomeViewState extends State<HomeView> {
           itemBuilder: (context, index) {
             final card = CategoryCards.cards(CategoryTitles.homeCategory[index]);
             return GestureDetector(
-              onTap: () {
+              onTap: () async {
                 _bloc.add(
                   QuestionEvent.fetchQuestionStart(
                     InterviewCategories.categories.toList()[index],
+                    context.locale.languageCode,
                   ),
                 );
                 setState(() => category = card!.category);
